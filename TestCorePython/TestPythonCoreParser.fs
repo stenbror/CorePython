045768a9b7c644ea547e2eae7f29c507b64a1392
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-11 21:14:22 +0100

    Unittests for power and mul.

--- TestCorePython/TestPythonCoreParser.fs
+++ TestCorePython/TestPythonCoreParser.fs
@@ -4,6 +4,9 @@ open System
 open Xunit
 open CorePython.PythonCoreParser
 
+
+// UnitTests for reserved keywords ////////////////////////////////////////////////////////////////////////////////////
+
 [<Fact>]
 let ``Reserved keyword 'False'`` () =
     let res = IsReservedKeywordSymbol("False", 11u)
@@ -18,3 +21,27 @@ let ``Reserved keyword 'None'`` () =
 let ``Reserved keyword 'True'`` () =
     let res = IsReservedKeywordSymbol("True", 11u)
     Assert.True( match res with | Some(Symbol.PyTrue(11u, 15u)) -> true | _ -> false )
+    
+    
+    
+// UnitTests for operators and delimiters /////////////////////////////////////////////////////////////////////////////   
+    
+[<Fact>]
+let ``Operator or delimiter '**='`` () =
+    let res = IsOperatorOrDelimiterSymbol( ( '*', '*', '=' ), 5u )
+    Assert.True( match res with | Some( Symbol.PyPowerAssign(5u, 8u), 3uy ) -> true | _ -> false )
+    
+[<Fact>]
+let ``Operator or delimiter '**'`` () =
+    let res = IsOperatorOrDelimiterSymbol( ( '*', '*', ' ' ), 5u )
+    Assert.True( match res with | Some( Symbol.PyPower(5u, 7u), 2uy ) -> true | _ -> false )
+    
+[<Fact>]
+let ``Operator or delimiter '*='`` () =
+    let res = IsOperatorOrDelimiterSymbol( ( '*', '=', ' ' ), 5u )
+    Assert.True( match res with | Some( Symbol.PyMulAssign(5u, 7u), 2uy ) -> true | _ -> false )
+    
+[<Fact>]
+let ``Operator or delimiter '*'`` () =
+    let res = IsOperatorOrDelimiterSymbol( ( '*', ' ', ' ' ), 5u )
+    Assert.True( match res with | Some( Symbol.PyMul(5u, 6u), 1uy ) -> true | _ -> false )
